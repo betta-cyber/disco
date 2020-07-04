5fe6ec7382643766afb36dc713fcbf7c9f0911f1
betta-cyber <l21828604@gmail.com>
2020-07-04 23:12:05 +0800

    add web

--- src/mail.rs
+++ src/mail.rs
@@ -1,27 +1,46 @@
-use lettre::{EmailTransport, SmtpTransport};
-use lettre_email::EmailBuilder;
-use std::path::Path;
+use lettre::{ClientSecurity, ClientTlsParameters, SmtpClient, Transport};
+use lettre::smtp::{authentication::{Credentials, Mechanism}, ConnectionReuseParameters};
+use native_tls::{Protocol, TlsConnector};
+use lettre_email::Email;
+use log::{ info, error };
+use std::time::Duration;
 
-let email = EmailBuilder::new()
-    // Addresses can be specified by the tuple (email, alias)
-    .to(("user@example.org", "Firstname Lastname"))
-    // ... or by an address only
-    .from("user@example.com")
-    .subject("Hi, Hello world")
-    .text("Hello world.")
-    .build()
-    .unwrap();
+pub fn send_mail() {
+    let email = Email::builder()
+        // Addresses can be specified by the tuple (email, alias)
+        .to(("21828604@qq.com", "betta"))
+        .from("lightstrawberry@163.com")
+        .subject("Hi, Hello world")
+        .text("Hello world.")
+        .build();
 
-// Open a local connection on port 25
-let mut mailer = SmtpTransport::builder_unencrypted_localhost().unwrap()
-                                                                   .build();
-// Send the email
-let result = mailer.send(&email);
+    // Open a local connection on port 25
+    let creds = Credentials::new(
+        "lightstrawberry@163.com".to_string(),
+        "YOYGDONXCFSIPKOL".to_string(),
+    );
 
-if result.is_ok() {
-    println!("Email sent");
-} else {
-    println!("Could not send email: {:?}", result);
+    let smtp_host = "smtp.163.com".to_string();
+    let smtp_port = "25".to_string();
+
+    let addr = String::from(format!("{}:{}", smtp_host, smtp_port));
+    // let connector = TlsConnector::new().unwrap();
+    // let tls_params = ClientTlsParameters::new(smtp_host, connector);
+    // let security = ClientSecurity::Required(tls_params);
+    info!("kkkkkk");
+    let mut mailer = match SmtpClient::new_simple(&smtp_host) {
+    // let mut mailer = match SmtpClient::new(addr, security) {
+        Ok(v) => v.credentials(creds).timeout(Some(Duration::new(1, 0))).transport(),
+        Err(e) => {
+            error!("Connecting to SMTP server was error: {}", e);
+            return;
+        }
+    };
+
+    info!("start send email");
+    match mailer.send(email.unwrap().into()) {
+        Ok(_) => info!("Sending email success!"),
+        Err(e) => error!("Sending email was error: {}", e),
+    }
 }
 
-assert!(result.is_ok());
